91608d5e2608010b1c9b941ee046784f4992a801
Daniel <cawasdn@gmail.com>
2026-03-30 11:23:08 +0800

    first commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PTIS</title>
+</head>
+
+<body>
+
+</body>
+<h1>hello</h1>
+
+
+</html>
